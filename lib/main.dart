aa765d5f7efa354b8041ef24a2abac26c4552a42
younghero <yxzhang@hitotek.com>
2023-12-25 12:24:17 +0800

    feat: dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,67 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(MaterialApp(home: Home()));
+  runApp(const MaterialApp(home: Home()));
 }
 
 class Home extends StatelessWidget {
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My First App'),
-        backgroundColor: Colors.blue.shade300,
+        title: const Text('Dice'),
+        backgroundColor: Colors.red,
+        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20),
       ),
-      backgroundColor: Colors.lightBlue,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.lightBlue,
-                backgroundImage: AssetImage('assets/christmas.png')),
-            const Text('Christmas Deer',
-                style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white)),
-            const Text(
-              'Made with Flutter',
-              style: TextStyle(
-                  color: Colors.tealAccent,
-                  fontSize: 30,
-                  letterSpacing: 2,
-                  fontWeight: FontWeight.bold),
+      backgroundColor: Colors.red,
+      body: const DicePage(),
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 2;
+  int rightDiceNumber = 2;
+
+  void changeDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () => {
+                changeDice()
+              },
+              child: Image.asset('assets/dice$leftDiceNumber.png'),
             ),
-            SizedBox(
-              height: 10,
-              width: 200,
-              child: Divider(color: Colors.lightBlue.shade300),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () => {
+                changeDice()
+              },
+              child: Image.asset('assets/dice$rightDiceNumber.png'),
             ),
-            Card(
-                color: Colors.white,
-                margin: const EdgeInsets.all(20),
-                child: ListTile(
-                    leading: Icon(
-                      Icons.phone,
-                      color: Colors.lightBlue.shade500,
-                    ),
-                    title: Text(
-                      '+ 19857120576',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.lightBlue.shade500,
-                      ),
-                    ))),
-            Card(
-                color: Colors.white,
-                margin: const EdgeInsets.symmetric(vertical: 1, horizontal: 20),
-                child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      color: Colors.lightBlue.shade500,
-                    ),
-                    title: Text(
-                      'example@gmail.com',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.lightBlue.shade500,
-                      ),
-                    )))
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () => {print('click')},
-          backgroundColor: Colors.blue.shade300,
-          child: const Text('click',
-              style: TextStyle(
-                color: Colors.white,
-              ))),
     );
   }
 }
